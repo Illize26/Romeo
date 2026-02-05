5ef832fdf28889b17ec29ad703b8626c95af6770
Illize26 <cardenas.sebastian@gmail.com>
2026-02-05 12:26:20 +0100

    Memorial Romeo - Video YouTube embed funzionante

--- bunny-animation.js
+++ bunny-animation.js
@@ -1,4 +1,14 @@
-// El video de YouTube se reproduce automáticamente con los parámetros en la URL
+ï»¿// Memorial para Romeo - Video de YouTube
 console.log('Memorial para Romeo cargado');
+
+// Verifica si hay errores con el iframe
+window.addEventListener('load', () => {
+    const iframe = document.getElementById('video');
+    console.log('Iframe cargado:', iframe.src);
+});
+
+
+
+
 
 
